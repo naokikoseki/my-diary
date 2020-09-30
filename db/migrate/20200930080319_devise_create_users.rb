50f13a43b665f7f425369c38b6f8fc83d7415294
naokikoseki <naoki.koseki0220@gmail.com>
2020-10-01 00:24:18 +0900

    ログイン/サインイン画面&ユーザーテーブル修正

--- db/migrate/20200930080319_devise_create_users.rb
+++ db/migrate/20200930080319_devise_create_users.rb
@@ -6,7 +6,6 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       ## Database authenticatable
       t.string :nickname,           null: false
       t.string :email,              null: false, default: ""
-      t.date   :birthday,           null:false
       t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
--- db/schema.rb
+++ db/schema.rb
@@ -36,7 +36,6 @@ ActiveRecord::Schema.define(version: 2020_09_30_080319) do
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
     t.string "email", default: "", null: false
-    t.date "birthday", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
