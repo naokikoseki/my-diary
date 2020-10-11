019f4a3c68de396170dbffbd380ff35d110d2083
naokikoseki <naoki.koseki0220@gmail.com>
2020-10-11 21:10:18 +0900

    画面遷移の制限（セキュリティ上）

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!, except: :community
+  before_action :authenticate_user!, except: [:community, :show]
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
